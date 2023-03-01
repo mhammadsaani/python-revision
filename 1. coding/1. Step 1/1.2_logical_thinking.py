04d7bc60853cfeda0511e18d9f9e0cbcf581a3ed
Muhammad Hammad Sani <m.hammadsaani@gmail.com>
2023-03-02 02:36:33 +0500

    pattern 17 done

--- 1. coding/1. Step 1/1.2_logical_thinking.py
+++ 1. coding/1. Step 1/1.2_logical_thinking.py
@@ -1,4 +1,5 @@
 
+import string
 class Patterns:
     
     
@@ -92,7 +93,58 @@ class Patterns:
                     else:
                         print("1 ", end="")
             print()
+        
+    def pattern_12(self, n):
+        for row in range(n-1):
+            for col in range(row+1):
+                print(col+1, end="")
+            
+            for col in range(2 * (n - 1) - 2 * (row + 1)):
+                print(" ", end="")
+            
+            for col in range(row+1, 0, -1):
+                print(col, end="")
+            
+            print()
+            
+    def pattern_13(self, n):
+        num = 0
+        for row in range(n):
+            for col in range(row+1):
+                num += 1
+                print(f'{num} ', end="")
+            print()
+            
+    def pattern_14(self, n):
+        for row in range(1, n+1):
+            print(string.ascii_uppercase[0:row])
+            
+    def pattern_15(self, n):
+        for row in range(1, n+1):
+            print(string.ascii_uppercase[0: n + 1 - row])
+                
+    def pattern_16(self, n):
+        for row in range(1, n+1):
+            for col in range(row):
+                print(string.ascii_uppercase[row-1], end="")
+            print()
+            
+    def pattern_17(self, n):
+        for row in range(1, n+1):
+            for col in range(n - row):
+                print(" ", end="")
+            for col in range(row):
+                print(string.ascii_uppercase[col], end="")
+            for col in range(row-1, 0, -1):
+                print(string.ascii_uppercase[col-1], end="")
+            for col in range(n - row):
+                print(" ", end="")
+            print()
+        
+    def pattern_18(self, n):
+        for row in range(1, n+1):
+            print(string.ascii_uppercase[n - row])
 
 
 pattern = Patterns()
-pattern.pattern_11(10)
+pattern.pattern_17(4)
---
+++ 1. coding/1. Step 1/test.py
@@ -0,0 +1,31 @@
+import collections
+
+_list = [x *x
+         for x in range(10)
+         if x % 2 == 0]
+
+_set = {x *x
+         for x in range(10)
+         if x % 2 == 0}
+
+_dict = {x: x*x
+         for x in range(10)
+         if x % 2 == 0}
+
+temp = {}
+for x in range(10):
+    if x % 2 == 0:
+        temp[x] = x*x
+
+print(_list)
+print(_set)
+print(temp, _dict)
+
+
+tuples = [(1, 'd'), (2, 'b'), (4, 'a'), (3, 'c')]
+result = sorted(tuples, key=lambda x: x[1])
+# [(4, 'a'), (2, 'b'), (3, 'c'), (1, 'd')]
+print(result)
+
+import string
+print(type(string.ascii_uppercase))
